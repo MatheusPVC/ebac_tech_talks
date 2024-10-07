753f1a82e80103485c2db787a059898ec10c720e
madson <matheuscuridemp3@gmail.com>
2024-10-06 22:43:44 -0300

    lista de eventos e contagem regressiva para o evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('Hello everybody');
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampDoAtual = agora.getTime();
+
+    const distanciaDoEvento = timeStampDoEvento - timeStampDoAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaDoEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaDoEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaDoEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaDoEvento % minutoEmMs) / 1000);
+
+
+    console.log(diasAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteOEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if (distanciaDoEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = '<p style="color: red">EVENTO EXPIRADO</p>'
+    }
+
+}, 1000);
